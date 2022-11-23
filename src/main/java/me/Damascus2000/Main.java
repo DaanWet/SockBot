88f1efba417cba1606b51335cb58ba9ee4959334
DaanWet <daan.wettinck@hotmail.com>
2022-11-23 11:18:54 +0100

    Add Pronostiek functionality

--- src/main/java/me/Damascus2000/Main.java
+++ src/main/java/me/Damascus2000/Main.java
@@ -11,17 +11,21 @@ import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello world!");
         JDA jda = JDABuilder.createDefault(args[0]).build();
-        jda.addEventListener(new TestCommand());
-        jda.addEventListener(new ButtonListener());
+        jda.addEventListener(new Pronostieken());
         CommandListUpdateAction commands = jda.updateCommands();
 
 
-        commands.addCommands(Commands.slash("test", "this is a first test command")
-                                                 .addOptions(new OptionData(OptionType.STRING, "ello", "some description idk"))
-                                                 .setGuildOnly(true)
-                                                 .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_SERVER))).queue();
-        commands.addCommands(Commands.slash("reac", "addrow").addOptions(new OptionData(OptionType.STRING ,"id", "id")).setGuildOnly(true).setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_SERVER))).queue();
+        commands.addCommands(Commands.slash("pronostiek", "this is a first test command")
+                                     .addOptions(new OptionData(OptionType.STRING , "date", "Date of the match", true),
+                                                 new OptionData(OptionType.STRING, "emoji_a", "Emoji for first country", true),
+                                                 new OptionData(OptionType.STRING, "country_a", "First country", true),
+                                                 new OptionData(OptionType.STRING, "emoji_b", "Emoji for second country", true),
+                                                 new OptionData(OptionType.STRING, "country_b", "Second country", true),
+                                                 new OptionData(OptionType.STRING, "image", "Image of the embed", true),
+                                                 new OptionData(OptionType.STRING, "question", "Extra question", true),
+                                                 new OptionData(OptionType.NUMBER, "hours", "How long to keep it open", true))
+                                     .setGuildOnly(true)
+                                     .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_SERVER))).queue();
     }
 }
---
+++ src/main/java/me/Damascus2000/Pronostieken.java
@@ -0,0 +1,88 @@
+package me.Damascus2000;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
+import net.dv8tion.jda.api.interactions.components.text.TextInput;
+import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
+import net.dv8tion.jda.api.interactions.modals.Modal;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Pronostieken extends ListenerAdapter {
+
+    private HashMap<String, LocalDateTime> dates;
+    private HashMap<String, ArrayList<Long>> pronostieken;
+
+    public Pronostieken(){
+        this.dates = new HashMap<>();
+        this.pronostieken = new HashMap<>();
+    }
+
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event){
+        String userid = event.getUser().getId();
+        if (event.getName().equals("pronostiek")){
+            String descr = String.format("%s %s - %s %s", event.getOption("emoji_a").getAsString(), event.getOption("country_a").getAsString(),
+                                          event.getOption("country_b").getAsString(), event.getOption("emoji_b").getAsString());
+            EmbedBuilder eb = new EmbedBuilder();
+            String date = event.getOption("date").getAsString();
+            eb.setTitle(String.format("WK Voetbal %s", date));
+            eb.setDescription(String.format("%s\nGeef jouw pronostiek en maak kans op een **gratis** consumptie. Dit sluit <t:%s:R>", descr, LocalDateTime.now().plusHours(event.getOption("hours").getAsInt()).toEpochSecond(ZoneOffset.UTC)));
+            eb.setImage(event.getOption("image").getAsString());
+            dates.put(date, LocalDateTime.now().plusHours(event.getOption("hours").getAsInt()));
+            pronostieken.put(date, new ArrayList<>());
+            event.replyEmbeds(eb.build()).addActionRow(Button.primary(
+                    String.format("%s_%s_%s", event.getOption("date").getAsString(), descr, event.getOption("question").getAsString()),
+                    "\uD83D\uDCDD Geef pronostiek")).queue();
+
+        }
+    }
+
+    @Override
+    public void onButtonInteraction(ButtonInteractionEvent event){
+        String[] split = event.getComponentId().split("_");
+        if (dates.get(split[0]).isAfter(LocalDateTime.now())){
+            if (pronostieken.get(split[0]).contains(event.getUser().getIdLong())){
+                event.reply("Je hebt je pronostiek al gegeven").setEphemeral(true).queue();
+                return;
+            }
+            TextInput t = TextInput.create("prono", "Wat is jouw pronostiek?", TextInputStyle.SHORT).build();
+            TextInput t2 = TextInput.create("extra" , split[2], TextInputStyle.SHORT).build();
+
+            Modal m = Modal.create(event.getComponentId(), split[1]).addActionRows(ActionRow.of(t), ActionRow.of(t2)).build();
+            event.replyModal(m).queue();
+        } else {
+            Button button = event.getButton().asDisabled();
+            // edit button?
+            event.reply("De tijd is verstreken").setEphemeral(true).queue();
+        }
+    }
+
+    @Override
+    public void onModalInteraction(ModalInteractionEvent event){
+        String[] split = event.getModalId().split("_");
+        pronostieken.get(split[0]).add(event.getUser().getIdLong());
+        EmbedBuilder eb = new EmbedBuilder();
+        eb.setAuthor(event.getMember().getEffectiveName(), null, event.getUser().getAvatarUrl());
+        eb.setTitle(split[1]);
+        eb.addField("Pronostiek", event.getValue("prono").getAsString(), true);
+        eb.addField(split[2], event.getValue("extra").getAsString(), true);
+        event.replyEmbeds(eb.build()).queue();
+    }
+
+
+
+
+
+
+}
